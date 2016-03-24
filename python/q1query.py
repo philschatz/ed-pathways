093687df98d3f5fe5f8dca1c8ef7105ea8b003f1
Danny Hendrix <dhendrix@schn.com>
2016-03-24 11:23:34 -0700

    Remove commas from performance field on SQL pull

--- python/q1query.py
+++ python/q1query.py
@@ -12,7 +12,7 @@ con = lite.connect('q1tableimport.db')
 #results = con.execute('select * from Performance')
 
 # here is the actual query
-results = con.execute('Select Summer_2015.SchoolID, Performance.School, Summer_2015.Activities, Performance.Subject, Performance.ParticipationRate2014to2015, Performance.Level3or4in2014to2015 from Summer_2015 Inner Join Performance on Summer_2015.SchoolID=Performance.SchoolID where Performance.Subgroup="Total Population"')
+results = con.execute('Select Summer_2015.SchoolID, Performance.School, replace(Summer_2015.Activities, ",", "") as "Summer_15.Activities", Performance.Subject, Performance.ParticipationRate2014to2015, Performance.Level3or4in2014to2015 from Summer_2015 Inner Join Performance on Summer_2015.SchoolID=Performance.SchoolID where Performance.Subgroup="Total Population"')
 
 all_rows = results.fetchall()
 
