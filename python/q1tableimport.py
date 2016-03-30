23ae5d8dcfe1b7b24c8f502c176323665a6397fa
slewis <slewis@composent.com>
2016-03-29 20:12:30 -0700

    Added RCmediaSchoolsAggregate dataset (note that RCmediaSchoolsAggregate_ORIG is the original from Katie Lakey, while RCmediaSchoolsAggregate.csv is slewis-modified version of data with column title spaces removed, % removed, Not Rated removed, * removed, -- removed and other general cleanup

--- python/q1tableimport.py
+++ python/q1tableimport.py
@@ -10,7 +10,7 @@ import pandas as pd
 performance = ('../data/Performance by School_Lang. Arts, Math .csv','Performance')
 summer2015 = ('../data/Summer_2015.csv','Summer_2015')
 oask = ('../data/OASK_DB.csv','OASK')
-
+rcmediaschoolsaggregate = ('../data/RCmediaSchoolsAggregate.csv','RCmediaSchoolsAggregate')
 #output file name
 outputfilename = 'q1tableimport'
 
@@ -18,6 +18,7 @@ outputfilename = 'q1tableimport'
 performancedt = pd.read_csv(performance[0],dtype={ 'SchoolID': str, 'ParticipationRate2014to2015': pd.np.float64, 'Level3or4in2014to2015': pd.np.float64})
 summer2015dt = pd.read_csv(summer2015[0], dtype={ 'SchoolID': str })
 oaskdt = pd.read_csv(oask[0], dtype={ 'SchoolID': str })
+rcmediaschoolsaggregatedt = pd.read_csv(rcmediaschoolsaggregate[0], dtype={ 'SchoolID': str})
 
 #open db connection (create)
 con = lite.connect(outputfilename+'.db')
@@ -26,6 +27,7 @@ con = lite.connect(outputfilename+'.db')
 performancedt.to_sql(performance[1], con, if_exists='replace')
 summer2015dt.to_sql(summer2015[1], con, if_exists='replace')
 oaskdt.to_sql(oask[1], con, if_exists='replace')
+rcmediaschoolsaggregatedt = rcmediaschoolsaggregatedt.to_sql(rcmediaschoolsaggregate[1], con, if_exists='replace')
 
 # create ddl 
 with open(outputfilename+'.sql', 'w') as f:
